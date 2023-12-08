0ca779ab2be0a563c398caed6195238bf8f0aad5
Pablo Duque <pabloduquesantos@gmail.com>
2023-12-08 12:55:19 -0300

    Correção dos nomes

--- login.php
+++ login.php
@@ -8,8 +8,8 @@
         $result = "SELECT * FROM USUARIO WHERE EMAIL = '$email' LIMIT 1";
         $resultado = mysqli_query($mysqli, $result);
         if ($resultado && $usuario = mysqli_fetch_assoc ($resultado)) {
-            if (password_verify($senha, $usuario["SENHA"])) {
-                if (password_needs_rehash($usuario["SENHA"], PASSWORD_DEFAULT)) {
+            if (password_verify($senha, $usuario["senha"])) {
+                if (password_needs_rehash($usuario["senha"], PASSWORD_DEFAULT)) {
                     $hashAtualizado = password_hash($senha, PASSWORD_DEFAULT);// Atualiza o hash 
                     $atualizaSenha = "UPDATE USUARIO SET SENHA = '$hashAtualizado' WHERE EMAIL = '$email'";//Insere hash atualizado no banco
                     $resultado = mysqli_query($mysqli, $atualizaSenha);
@@ -25,20 +25,20 @@
         $resultado = mysqli_query($mysqli, $result);
 
         if ($resultado && $usuario = mysqli_fetch_assoc($resultado)) {
-            if (password_verify($senha, $usuario["SENHA"])) {
-                if (password_needs_rehash($usuario["SENHA"], PASSWORD_DEFAULT)) {
+            if (password_verify($senha, $usuario["senha"])) {
+                if (password_needs_rehash($usuario["senha"], PASSWORD_DEFAULT)) {
                     $hashAtualizado = password_hash($senha, PASSWORD_DEFAULT);// Atualiza o hash 
                     $atualizaSenha = "UPDATE USUARIO SET SENHA = '$hashAtualizado' WHERE EMAIL = '$email'";//Insere hash atualizado no banco
                     $resultado = mysqli_query($mysqli, $atualizaSenha);
                 }
-                $_SESSION["email"] = $usuario["EMAIL"];
-                $_SESSION["nome"] = $usuario["NOME"];
-                $_SESSION["sobreNome"] = $usuario["SOBRENOME"];
-                $_SESSION["foto"] = $usuario["FOTO"];
+                $_SESSION["email"] = $usuario["email"];
+                $_SESSION["nome"] = $usuario["Nome"];
+                $_SESSION["sobreNome"] = $usuario["Sobrenome"];
+                $_SESSION["foto"] = $usuario["foto"];
                 /*inserir if(campo adm == 1 ){
                     direcionar a pagina de adms com cadastro de perguntas
                 }*/
-                header("Location: inicio.php");//Entrar na tela de inicio
+                header("Location: index.php");//Entrar na tela de inicio
             } else {
                 header("Location: login_tela.php");
                 $_SESSION['loginErro'] = 'Email ou senha inválido!';
