0010e8d5f4371709d0c5451109221aba2c95a651
Pablo Duque <pabloduquesantos@gmail.com>
2023-12-10 20:01:19 -0300

    Questões adicionadas

--- MySQL/enade.sql
+++ MySQL/enade.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03/12/2023 às 14:39
+-- Tempo de geração: 10/12/2023 às 23:59
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -31,7 +31,7 @@ CREATE TABLE `alternativa` (
   `id` int(11) NOT NULL,
   `alternativa` int(11) NOT NULL,
   `pergunta` int(11) NOT NULL,
-  `texto` tinytext NOT NULL
+  `texto` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -48,7 +48,32 @@ INSERT INTO `alternativa` (`id`, `alternativa`, `pergunta`, `texto`) VALUES
 (7, 2, 2, 'Uma das vantagens mais significativas de uma MV é a economia de carga de CPU e de memória RAM na execução de um aplicativo.'),
 (8, 3, 2, 'Uma MV oferece maior controle de segurança, uma vez que aplicativos são executados em um ambiente controlado.'),
 (9, 4, 2, 'Para emular uma CPU dual-core, uma MV deve ser instalada e executada em um computador com CPU dual-core.'),
-(10, 5, 2, 'Como uma MV não é uma máquina real, um sistema operacional nela executado fica automaticamente imune a vírus.');
+(10, 5, 2, 'Como uma MV não é uma máquina real, um sistema operacional nela executado fica automaticamente imune a vírus.'),
+(11, 1, 3, 'Sobrecarga.'),
+(12, 2, 3, 'Herança.'),
+(13, 3, 3, 'Sobreposição.'),
+(14, 4, 3, 'Abstração.'),
+(15, 5, 3, 'Mensagem.'),
+(21, 1, 4, 'I e II.'),
+(22, 2, 4, 'I e III.'),
+(23, 3, 4, 'II e III.'),
+(24, 4, 4, 'II e IV.'),
+(25, 5, 4, 'III e IV.'),
+(26, 1, 5, 'O software deve ser operacionalizado no sistema Linux.'),
+(27, 2, 5, 'O tempo de desenvolvimento não deve ultrapassar seis meses.'),
+(28, 3, 5, 'O software deve emitir relatórios de compras a cada quinze dias.'),
+(29, 4, 5, 'O tempo de resposta do sistema não deve ultrapassar 30 segundos.'),
+(30, 5, 5, 'A base de dados deve ser protegida para acesso apenas de usuários autorizados.'),
+(36, 1, 6, 'char alternar_estampa(char estampa, char *proxima) {<br> \r\n *proxima = *proxima == estampa ? \' \' : estampa;<br>\r\n return *proxima; <br>\r\n} <br>\r\nvoid criar_molde(char molde[][50], int c, int l, char estampa) { <br>\r\n char proxima = estampa; <br>\r\n for (int i = 0; i < l; i++) <br>\r\n for (int j = 0; j < c; j++) <br>\r\n molde[i][j] = alternar_estampa(estampa, &proxima); <br>\r\n}'),
+(37, 2, 6, 'char alternar_estampa(char estampa, char proxima) { <br>\r\n proxima = proxima == estampa ? \' \' : estampa; <br>\r\n return proxima; <br>\r\n} <br>\r\nvoid criar_molde(char molde[][50], int c, int l, char estampa) { <br>\r\n char proxima = estampa; <br>\r\n for (int i = 0; i < l; i++) <br>\r\n for (int j = 0; j < c; j++) <br>\r\n molde[i][j] = alternar_estampa(estampa, proxima); <br>\r\n}'),
+(38, 3, 6, 'char alternar_estampa(char estampa, char *proxima) { <br>\r\n return *proxima == estampa ? \' \' : estampa; <br>\r\n} <br>\r\nvoid criar_molde(char molde[][50], int c, int l, char estampa) { <br>\r\n char proxima = estampa; <br>\r\n for (int i = 0; i < l; i++) <br>\r\n for (int j = 0; j < c; j++) <br>\r\n molde[i][j] = alternar_estampa(estampa, &proxima); <br>\r\n} '),
+(39, 4, 6, 'char alternar_estampa(char estampa, char *proxima) { <br>\r\n *proxima = *proxima == estampa ? \' \' : estampa; <br>\r\n return *proxima; <br>\r\n} <br>\r\nvoid criar_molde(char molde[][50], int c, int l, char estampa) { <br>\r\n char proxima = estampa; <br>\r\n for (int i = 0; i < l; i++) { <br>\r\n for (int j = 0; j < c; j++) <br>\r\n molde[i][j] = alternar_estampa(estampa, &proxima); <br>\r\n proxima = molde[i][0]; <br>\r\n } <br>\r\n}'),
+(40, 5, 6, 'char alternar_estampa(char estampa, char proxima) { <br>\r\n proxima = proxima == estampa ? \' \' : estampa; <br>\r\n return proxima; <br>\r\n} <br>\r\nvoid criar_molde(char molde[][50], int c, int l, char estampa) {<br>\r\n char proxima = estampa; <br>\r\n for (int i = 0; i < l; i++) { <br>\r\n for (int j = 0; j < c; j++) <br>\r\n molde[i][j] = alternar_estampa(estampa, proxima); <br>\r\n proxima = molde[i][0]; <br>\r\n } <br>\r\n}\r\n'),
+(41, 1, 7, 'II.'),
+(42, 2, 7, 'I e III.'),
+(43, 3, 7, 'I, II e IV.'),
+(44, 4, 7, 'I, III e IV.'),
+(45, 5, 7, 'II, III e IV.');
 
 -- --------------------------------------------------------
 
@@ -107,8 +132,7 @@ INSERT INTO `conteudo` (`ID`, `nome`, `materia`) VALUES
 (38, 'Abstração de dados', 'LPR'),
 (39, 'Classes e suas características', 'LPR'),
 (40, 'Interfaces', 'LPR'),
-(41, 'Model-View-Controller', 'LPR'),
-(42, 'História', 'CTGR');
+(41, 'Model-View-Controller', 'LPR');
 
 -- --------------------------------------------------------
 
@@ -129,7 +153,6 @@ INSERT INTO `materia` (`sigla`, `nome`) VALUES
 ('ADRD', 'Administração de Redes'),
 ('ARQS', 'Arquitetura de Software'),
 ('BDA', 'Banco de Dados'),
-('CTGR', 'Conteúdo Geral'),
 ('DDMO', 'Desenvolvimento para Dispositivos Móveis'),
 ('ENSW', 'Engenharia de Software'),
 ('ESD', 'Estrutura de Dados'),
@@ -157,8 +180,13 @@ CREATE TABLE `pergunta` (
 --
 
 INSERT INTO `pergunta` (`id`, `enunciado`, `materia`, `conteudo`, `ano`, `gabarito`, `PONTUACAO`) VALUES
-(1, 'Um analista foi contratado para desenvolver um sistema de pesquisa de DVDs em lojas virtuais. O sistema deverá solicitar ao usuário um título de DVD, que será usado para realizar a pesquisa nas bases de dados das lojas conveniadas. Ao detectar a disponibilidade do DVD solicitado, o sistema armazenará temporariamente os dados das lojas (nome, preço, data prevista para entrega do produto) e exibirá as informações ordenadas por preço. Após analisar as informações, o cliente poderá efetuar a compra. O contratante deverá testar algumas operações do sistema antes de ele ser finalizado. Há tempo suficiente para que o analista atenda a essa solicitação e efetue eventuais modificações exigidas pelo contratante. <br><br>Com relação a essa situação, julgue os itens a seguir quanto ao modelo de ciclo de vida.', 'ENSW', 19, 2008, 4, NULL),
-(2, 'O conceito de máquina virtual (MV) foi usado na década de 70 do século passado no sistema operacional IBM System 370. Atualmente, centros de dados (datacenters) usam MVs para migrar tarefas entre servidores conectados em rede e, assim, equilibrar carga de processamento. Além disso, plataformas atuais de desenvolvimento de software empregam MVs (Java, .NET). Uma MV pode ser construída para emular um processador ou um computador completo. Um código desenvolvido para uma máquina real pode ser executado de forma transparente em uma MV. <br><br>Com relação a essas informações, assinale a opção correta.', 'ADRD', 2, 2008, 3, NULL);
+(1, 'Um analista foi contratado para desenvolver um sistema de pesquisa de DVDs em lojas virtuais. O sistema deverá solicitar ao usuário um título de DVD, que será usado para realizar a pesquisa nas bases de dados das lojas conveniadas. Ao detectar a disponibilidade do DVD solicitado, o sistema armazenará temporariamente os dados das lojas (nome, preço, data prevista para entrega do produto) e exibirá as informações ordenadas por preço. Após analisar as informações, o cliente poderá efetuar a compra. O contratante deverá testar algumas operações do sistema antes de ele ser finalizado. Há tempo suficiente para que o analista atenda a essa solicitação e efetue eventuais modificações exigidas pelo contratante. <br><br>Com relação a essa situação, julgue os itens a seguir quanto ao modelo de ciclo de vida.<br><br>I O entendimento do sistema como um todo e a execução seqüencial das fases sem retorno produzem um sistema que pode ser validado pelo contratante.<br><br>II A elaboração do protótipo pode ser utilizada para resolver dúvidas de comunicação, o que aumenta os riscos de inclusão de novas funcionalidades nãoprioritárias.<br><br>III A definição das restrições deve ser a segunda fase a ser realizada no desenvolvimento do projeto, correspondendo à etapa de engenharia.<br><br>IV Um processo iterativo permite que versões progressivas mais completas do sistema sejam construídas e avaliadas.', 'ENSW', 19, 2008, 4, NULL),
+(2, 'O conceito de máquina virtual (MV) foi usado na década de 70 do século passado no sistema operacional IBM System 370. Atualmente, centros de dados (datacenters) usam MVs para migrar tarefas entre servidores conectados em rede e, assim, equilibrar carga de processamento. Além disso, plataformas atuais de desenvolvimento de software empregam MVs (Java, .NET). Uma MV pode ser construída para emular um processador ou um computador completo. Um código desenvolvido para uma máquina real pode ser executado de forma transparente em uma MV. <br><br>Com relação a essas informações, assinale a opção correta.', 'ADRD', 2, 2008, 3, NULL),
+(3, 'Uma pizzaria fez uma ampliação de suas\r\ninstalações e o gerente aproveitou para melhorar o sistema\r\ninformatizado, que era limitado e não atendia a todas as\r\nfunções necessárias. O gerente, então, contratou uma\r\nempresa para ampliar o software. No desenvolvimento do\r\nnovo sistema, a empresa aproveitou partes do sistema\r\nantigo e estendeu os componentes de maneira a usar\r\ncódigo validado, acrescentando as novas funções\r\nsolicitadas.<br>Que conceito de orientação a objetos está descrito na\r\nsituação hipotética acima?', 'LPR', 37, 2008, 2, NULL),
+(4, 'Um analista foi contratado para desenvolver um\r\nsistema de pesquisa de DVDs em lojas virtuais. O sistema\r\ndeverá solicitar ao usuário um título de DVD, que será\r\nusado para realizar a pesquisa nas bases de dados das\r\nlojas conveniadas. Ao detectar a disponibilidade do DVD\r\nsolicitado, o sistema armazenará temporariamente os\r\ndados das lojas (nome, preço, data prevista para entrega\r\ndo produto) e exibirá as informações ordenadas por preço.\r\nApós analisar as informações, o cliente poderá efetuar a\r\ncompra. O contratante deverá testar algumas operações do\r\nsistema antes de ele ser finalizado. Há tempo suficiente\r\npara que o analista atenda a essa solicitação e efetue\r\neventuais modificações exigidas pelo contratante.\r\n<br>\r\nCom relação a essa situação, julgue os itens a seguir\r\nquanto ao modelo de ciclo de vida.<br><br>I O entendimento do sistema como um todo e a execução\r\nseqüencial das fases sem retorno produzem um sistema\r\nque pode ser validado pelo contratante.\r\n<br><br>\r\nII A elaboração do protótipo pode ser utilizada para\r\nresolver dúvidas de comunicação, o que aumenta os\r\nriscos de inclusão de novas funcionalidades nãoprioritárias.\r\n<br><br>\r\nIII A definição das restrições deve ser a segunda fase a\r\nser realizada no desenvolvimento do projeto,\r\ncorrespondendo à etapa de engenharia.\r\n<br><br>\r\nIV Um processo iterativo permite que versões progressivas\r\nmais completas do sistema sejam construídas e\r\navaliadas.', 'ENSW', 19, 2008, 4, NULL),
+(5, 'A etapa de definição de requisitos é voltada para estabelecer quais as funções são requeridas pelo sistema e as restrições sobre a operação e o desenvolvimento do software. Os requisitos de software podem ser classificados como requisitos funcionais e não funcionais.\r\nSOMMERVILLE, I. Engenharia de Software, 10.\r\ned. São Paulo: Pearson Education, 2019 (adaptado).\r\n<br><br>\r\nConsiderando as informações do texto, assinale a alternativa em que o item é um requisito funcional.', 'ENSW', 20, 2021, 3, NULL),
+(6, 'Uma fábrica adquiriu máquinas de costura industrial para a produção de jogos de toalhas de mesa e guardanapos de tecido com estampas quadriculadas. Cada quadrículo tem tamanho padrão de 5 cm X 5 cm. Para a configuração da costura, é preciso um programa de computador para criar um molde em que sejam informadas as quantidades de quadrículos por comprimento e por largura, além do desenho da estampa a ser costurada. Por exemplo, para um guardanapo de tecido que tenha 30 cm X 30 cm, com uma estampa representada pelo caractere “#”, o seguinte molde precisa ser criado:<br><br>\r\n\r\nComo uma toalha pode ter, no máximo, 250 cm de comprimento e 200 cm de largura, o programador declarou uma matriz char molde[40][50] para ser possível representar moldes cujas medidas compreendam esses limites. Ele também implementou, entre outras, as seguintes funções:<br><br>- criar_molde, para a qual os seguintes parâmetros são esperados, nessa ordem: uma referência à matriz molde, o comprimento e a largura do tecido (em quantidades de quadrículos) e o caractere que representa a estampa;\r\n<br>- alternar_estampa, que tem a tarefa de controlar a disposição alternada das estampas no molde.\r\n<br><br>\r\nConsiderando o cenário descrito, que alternativa a seguir apresenta a implementação correta das funções criar_molde e alternar_estampa?', 'LPR', 36, 2021, 3, NULL),
+(7, 'Uma fábrica de software está realizando entrevistas para contração de um profissional que esteja alinhado às exigências do atual mundo corporativo. Sabe-se que processos ágeis de desenvolvimento têm se tornado essenciais para empresas que desejam realizar entregas rápidas e frequentes de produtos e/ou serviços de software.\r\n\r\nEssa empresa possui uma equipe de desenvolvimento que faz uso de processos ágeis como o Scrum e eXtreme Programming (XP) e o acompanhamento por meio do quadro Kanban. Sendo assim, um conjunto de características deve ser verificado durante a entrevista para garantir que o candidato a ser contratado possua conhecimentos necessários para atuar juntamente a esta equipe.\r\n<br><br>\r\nCom base no texto e nos processos ágeis de desenvolvimento de software, avalie as afirmações a seguir.<br><br>I. Métodos Ágeis são baseados em ciclos iterativo e incremental que se concentram no desenvolvimento rápido e na flexibilidade às mudanças, com a participação do cliente no processo de software.\r\n\r\n<br><br>II. Uma forte característica da XP é a garantia da qualidade do código produzido e, para isso, os desenvolvedores produzem testes automatizados antes mesmo de codificar uma funcionalidade.\r\n\r\n<br><br>III. O planejamento no Scrum é baseado na elaboração dos itens do product backlog, que é uma lista de funcionalidades desejadas pelo cliente, sendo o Scrum Master o responsável por gerenciá-lo.\r\n\r\n<br><br>IV. O quadro Kanban permite monitorar a evolução das tarefas necessárias durante o processo ágil de desenvolvimento de software, possibilitando um acompanhamento de forma visual das atividades em construção.\r\n\r\nÉ correto apenas o que se afirma em: ', 'ENSW', 19, 2021, 5, NULL);
 
 -- --------------------------------------------------------
 
@@ -187,6 +215,13 @@ CREATE TABLE `usuario` (
   `email` varchar(255) NOT NULL,
   `adm` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuario`
+--
+
+INSERT INTO `usuario` (`Nome`, `Sobrenome`, `senha`, `foto`, `pontuacao_maxima`, `email`, `adm`) VALUES
+('Teste', 'Teste', '$2y$10$6YSsLOHYsmsKucHXdnWKWO.rIQrQpnf0PqnX5uDPTfCHjAdipHOJy', NULL, 0, 'Teste@gmail.com', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -240,7 +275,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `alternativa`
 --
 ALTER TABLE `alternativa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT de tabela `conteudo`
@@ -252,7 +287,7 @@ ALTER TABLE `conteudo`
 -- AUTO_INCREMENT de tabela `pergunta`
 --
 ALTER TABLE `pergunta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restrições para tabelas despejadas
